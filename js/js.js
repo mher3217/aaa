cd73420bdc4674ba85bef0c809e9cb081866a0e5
mher3217 <mher-harutyunyan92@mail.ru>
2018-05-07 12:49:59 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -29,10 +29,10 @@ pauseButton.addEventListener("click", function() {
   }
 }, false);
 
-video.addEventListener('touchstart', function(e) {
+/*video.addEventListener('touchstart', function(e) {
 e.preventDefault();
 video.play();
-})
+})*/
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
